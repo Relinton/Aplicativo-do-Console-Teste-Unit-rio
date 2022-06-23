5fbd0d7ab282f4308ac53b600c08e2e4c51f3d41
Relinton <relinton.pinheiro.infouniron@gmail.com>
2022-06-23 13:31:03 -0400

    Realizado Teste Unitário no Aplicativo do Console Calculos

---
+++ AppConsoleCalculos/Calculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConsoleCalculos
+{
+    public class Calculo
+    {
+        public static double Somar(double num1, double num2)
+        {
+            return (num1 + num2);
+        }
+        public static double Subtrair(double numero1, double numero2)
+        {
+            return (numero1 - numero2);
+        }
+        public static double Multiplicar(double numero1, double numero2)
+        {
+            return (numero1 * numero2);
+        }
+        public static double Dividir(double numero1, double numero2)
+        {
+            return (numero1 / numero2);
+        }
+        public static bool IsNumeroPar(int numero)
+        {
+            return numero % 2 == 0;
+        }
+    }
+}
---
+++ AppConsoleCalculos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppConsoleCalculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
